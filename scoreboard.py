e37d1773adb3bd36c9cf081b4accf592ce130568
Wen Kang Lin <161750659+WenKangLin@users.noreply.github.com>
2025-08-20 16:50:50 +0100

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -8,16 +8,16 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.color("white")
-        self.write(f'Score: {self.score}', align='center', font=('Courir', 20, 'normal'))
+        self.write(f'Score: {self.score}', align='center', font=('Courier', 20, 'normal'))
         self.goto(0, 270)
 
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f'Score: {self.score}', align='center', font=('Courir', 20, 'normal'))
+        self.write(f'Score: {self.score}', align='center', font=('Courier', 20, 'normal'))
 
     def game_over(self):
         self.clear()
         self.teleport(0,0)
-        self.write("Game Over!", align='center', font=('Courir', 20, 'bold'))
+        self.write("Game Over!", align='center', font=('Courier', 20, 'bold'))
 
